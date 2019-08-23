61594ca49653b82968199df18b63c8ec070a2daf
codeliling <maximuslee@126.com>
2019-08-23 14:09:56 +0800

    project init

--- app/controller/manage/manage.js
+++ app/controller/manage/manage.js
@@ -3,11 +3,13 @@
 const Controller = require('egg').Controller;
 
 class ManageController extends Controller {
-  async index() {
-    const { ctx } = this;
-    ctx.body = 'hi, egg';
-  }
 
+  async index(){
+      const ctx = this.ctx;
+      await ctx.render('manage/index.html', {
+        user : ctx.user
+      });
+  }
 
 }
 
