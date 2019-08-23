dc8759596366dec62266c2a05527a97553aee7b7
codeliling <maximuslee@126.com>
2019-08-23 12:52:18 +0800

    project init

--- config/config.default.js
+++ config/config.default.js
@@ -23,6 +23,43 @@ module.exports = appInfo => {
     // myAppName: 'egg',
   };
 
+  config.sequelize = {
+    dialect: 'mysql',
+    host: '192.168.3.110',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'xiaojian',
+    timezone:'+08:00',
+    logging: true,
+    define: {
+      freezeTableName: true,
+      charset: 'utf8',
+      dialectOptions: {
+        collate: 'utf8_general_ci',
+      },
+      timestamps: false,
+    },
+    pool: {
+      max: 5,
+      min: 1,
+      acquire: 30000,
+      idle: 10000
+    },
+  };
+
+  config.security = {
+    csrf:{
+      enable:false,
+      ignoreJSON:true
+    },
+    domainWhiteList: ['*']
+  };
+
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+  };
+
   return {
     ...config,
     ...userConfig,
--- config/plugin.js
+++ config/plugin.js
@@ -1,9 +1,27 @@
 'use strict';
 
 /** @type Egg.EggPlugin */
-module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
+exports.sequelize = {
+  enable: true,
+  package: 'egg-sequelize',
+};
+
+exports.cors = {
+  enable: true,
+  package: 'egg-cors',
+};
+
+exports.passport = {
+  enable: true,
+  package: 'egg-passport',
+};
+
+exports.passportLocal = {
+  enable: true,
+  package: 'egg-passport-local',
+};
+
+exports.nunjucks = {
+  enable: true,
+  package: 'egg-view-nunjucks',
 };
