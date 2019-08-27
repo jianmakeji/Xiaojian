baa129e57754a475a8ada08dac6e966780ad4401
gansiqiu <1223720954@qq.com>
2019-08-27 17:35:59 +0800

    添加人员管理，人员修改静态界面

--- app/router.js
+++ app/router.js
@@ -10,4 +10,7 @@ module.exports = app => {
   router.get('/manage/login', controller.manage.manage.login);
   router.resources('/manage/user',  controller.manage.user);
   router.get('/manage/courseManage', controller.manage.manage.courseManage);
+  router.get('/manage/personalManage', controller.manage.manage.personalManage);
+  router.get('/manage/editStoreUser', controller.manage.manage.editStoreUser);
+  router.get('/manage/userRecord', controller.manage.manage.userRecord);
 };
