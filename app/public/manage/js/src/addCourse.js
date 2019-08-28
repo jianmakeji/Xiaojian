87629d747c39b414c199864ed6f9f35acf6d497e
gansiqiu <1223720954@qq.com>
2019-08-28 16:27:04 +0800

    fix bug

--- app/public/manage/js/src/addCourse.js
+++ app/public/manage/js/src/addCourse.js
@@ -2,14 +2,33 @@ var index = new Vue({
     el:".index",
     data(){
         return{
-            courseTypeId:"",
-            courseTypeData:[
-                {value: '1',label: '儿童诗歌'},
-                {value: '2',label: '成语故事'},
-                {value: '2',label: '辅助课程'}
-            ],
-            searchCourseTypeValue:"",
+            // 上传参数对象
+            formItem:{
+                courseName:"",
+                courseAbstract:"",
+                courseThumbA:"",
+                courseThumbB:"",
+                h5Address:"",
+                courseType:"",
+                courseSubType:""
+            },
 
+            // 课程类别和标签数据
+            courseTypeData:config.globalData.courseTypeData,
+            courseSubTypeData:config.globalData.courseSubTypeData,
+
+            // 缩略图和h5临时变量
+            fileName_1:"",
+			imgUrl_1:"",
+			progressPercent_1:0,
+			fileName_2:"",
+			imgUrl_2:"",
+			progressPercent_2:0,
+			fileName_3:"",
+			imgUrl_3:"",
+			progressPercent_3:0,
+            attachFileName:"",
+            attachFilePercent:"",
         }
     },
     methods:{
@@ -26,6 +45,22 @@ var index = new Vue({
                     break;
                 default:
             }
+        },
+        // 上传缩略图
+        doUpload_1(files){
+
+        },
+        doUpload_2(files){
+
+        },
+        step2_upload_ZIP_change(){
+
+        },
+        courseTypeChange(){
+
+        },
+        courseSubTypeChange(){
+
         }
     },
     created(){
@@ -33,3 +68,36 @@ var index = new Vue({
         $(".menuBtns").children().eq(2).addClass('active');
     }
 })
+
+
+
+/**
+ * 文件名编码
+ */
+function random_string(len) {
+	len = len || 32;
+	var chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
+	var maxPos = chars.length;
+	var pwd = '';
+	for(let i = 0; i < len; i++) {
+		pwd += chars.charAt(Math.floor(Math.random() * maxPos));
+	}
+	return pwd;
+}
+
+function get_suffix(filename) {
+	let pos = filename.lastIndexOf('.');
+	let suffix = '';
+	if(pos != -1) {
+		suffix = filename.substring(pos);
+	}
+	return suffix;
+}
+
+function calculate_object_name(filename) {
+
+	let suffix = get_suffix(filename);
+	let g_object_name = random_string(10) + suffix;
+
+    return g_object_name;
+}
---
+++ app/public/manage/js/src/config.js
@@ -0,0 +1,29 @@
+var config = {
+    globalData:{
+        // 课程标签
+        courseTypeData:[
+            {value: '0',label: '基础课程'},
+            {value: '1',label: '辅导课程'},
+            {value: '2',label: '运动课程'}
+        ],
+        // 课程类别
+        courseSubTypeData:[
+            {value: '0',label: '成语故事'},
+            {value: '1',label: '儿童诗歌'},
+            {value: '2',label: '常识'},
+            {value: '3',label: '美术'},
+            {value: '4',label: '篮球'}
+        ],
+        // 店铺数据
+        storeData:[
+            {value: '0',label: '店铺A'},
+            {value: '1',label: '店铺B'},
+            {value: '2',label: '店铺C'},
+            {value: '3',label: '店铺D'},
+            {value: '4',label: '店铺E'}
+        ]
+    },
+    ajaxUrls:{
+
+    },
+}
