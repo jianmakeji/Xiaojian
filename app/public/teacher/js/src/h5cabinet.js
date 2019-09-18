7849e53e3c2ddf28cd03114b33a239410ae0cde8
gansiqiu <1223720954@qq.com>
2019-09-18 16:53:51 +0800

    fix bugs

--- app/public/teacher/js/src/h5cabinet.js
+++ app/public/teacher/js/src/h5cabinet.js
@@ -2,11 +2,45 @@ var index = new Vue({
     el:".index",
     data(){
         return{
-            h5Address:""
+            h5Address:[],
+            isActiveNum:0,
+            playH5Address:"",
+            courseType:1
         }
     },
     methods:{
+        clickNumH5(numH5){
+            this.isActiveNum = numH5;
+            this.playH5Address = this.h5Address[numH5];
+        },
+        // 上一个h5
+        clickPrevH5(){
+            if (this.isActiveNum > 0) {
+                this.isActiveNum--;
+                this.playH5Address = this.h5Address[this.isActiveNum];
+            }
+        },
+        // 下一个h5
+        clickNextH5(){
+            let h5Count = 3;
+            if (this.courseType != 1) {
+                h5Count = 1;
+            } else {
+                h5Count = 3;
+            }
+            if (this.isActiveNum < h5Count) {
+                this.isActiveNum++;
+                this.playH5Address = this.h5Address[this.isActiveNum];
+            }
+        },
+        // 播放h5
+        clickPlayH5(){
 
+        },
+        // 返回
+        clickBackH5(){
+            window.close();
+        }
     },
     created(){
         let that = this;
@@ -17,8 +51,9 @@ var index = new Vue({
         })
         .done(function(res) {
             if (res.status == 200) {
-                // console.log(res.data);
-                that.h5Address = res.data.h5Address;
+                that.h5Address = res.data.h5Address.split(",");
+                that.playH5Address = that.h5Address[0];
+                that.courseType = res.data.courseType;
             } else {
                 that.$Message.error(res.data);
             }
